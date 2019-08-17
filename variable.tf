8c1f0be0a3f9e3c38b64ec7628776c0d4f5166e2
Madina89 <madinalinux89@gmail.com>
2019-08-17 13:15:20 -0500

    updated

--- variable.tf
+++ variable.tf
@@ -1,5 +1,5 @@
 variable "master_instance_type"{}
 variable "node_instance_type" {}
-variabli "node_max_size"{}
-variable "node_min_size"{}  
+variabli "node_max_size" {}
+variable "node_min_size" {}  
 
